bca413af92ce38906853c9318020177ffc728e00
tsrshiv <shivendraverma111111@gmail.com>
2024-08-01 18:45:48 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: 
+            ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection falied !!!", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"; 
+// const app= express();
+
+// ;(async()=>{
+//     try {
+//       await mongoose.connect(`${process.env.
+//         MONGODB_URI}/${DB_NAME}`)  
+//         app.on("error",(error)=>{
+//             console.log("ERRROR:", error);
+//             throw error
+//     })
+
+//     app.listen(process.env.PORT, ()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//     })
+//     } catch (error) {
+//        console.log("ERROR: ",error) 
+//        throw err
+//     }
+// })()
